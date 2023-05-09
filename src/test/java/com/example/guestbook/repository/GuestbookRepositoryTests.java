315f1068270c6ef3837f4a5dd86b640ddc58b1ce
homoconsumus <rilid@naver.com>
2023-05-09 16:40:48 +0900

    guestbook basic 0509

--- src/test/java/com/example/guestbook/repository/GuestbookRepositoryTests.java
+++ src/test/java/com/example/guestbook/repository/GuestbookRepositoryTests.java
@@ -1,14 +1,28 @@
 package com.example.guestbook.repository;
 
+import com.example.guestbook.dto.GuestbookDTO;
+import com.example.guestbook.dto.PageRequestDTO;
+import com.example.guestbook.dto.PageResultDTO;
 import com.example.guestbook.entity.Guestbook;
+import com.example.guestbook.entity.QGuestbook;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
+import java.util.Optional;
 import java.util.stream.IntStream;
 
+@SpringBootTest
 public class GuestbookRepositoryTests {
     @Autowired
     private GuestbookRepository guestbookRepository;
+
 
     @Test
     public void insertDummies(){
@@ -19,6 +33,51 @@ public class GuestbookRepositoryTests {
                     .writer("user" + (i%10))
                     .build();
             System.out.println(guestbookRepository.save(guestbook));
+        });
+    }
+
+    @Test
+    public void updateTest(){
+        Optional<Guestbook> result = guestbookRepository.findById(300L); // id가 300인 행 수정하기 위해 존재여부 확인
+        if(result.isPresent()){
+            Guestbook guestbook = result.get();
+            guestbook.changeTitle("Changed title...");
+            guestbook.changeContent("Changed content...");
+            guestbookRepository.save(guestbook);
+        }
+    }
+
+    // Querydsl을 사용하여 특정 키워드를 가진 행들을 검색
+    @Test
+    public void testQuery(){
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("gno").descending());
+        QGuestbook qGuestbook = QGuestbook.guestbook;
+        String keyword = "7";
+        BooleanBuilder builder = new BooleanBuilder();
+        BooleanExpression expression = qGuestbook.title.contains(keyword);
+        builder.and(expression);
+        Page<Guestbook> result = guestbookRepository.findAll(builder, pageable);
+        result.stream().forEach(guestbook -> {
+            System.out.println(guestbook);
+        });
+    }
+
+    // Querydsl을 사용해서 다중 검색어 테스트
+    @Test
+    public void testQuery2(){
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("gno").ascending());
+        QGuestbook qGuestbook = QGuestbook.guestbook;
+        String keyword = "7";
+        BooleanBuilder builder = new BooleanBuilder();
+        BooleanExpression expTitle = qGuestbook.title.contains(keyword);
+        BooleanExpression expWriter = qGuestbook.writer.contains(keyword);
+        BooleanExpression expAll = expTitle.and(expWriter);
+        builder.and(expAll);
+        // builder.or(expAll);
+        builder.and(qGuestbook.gno.gt(50L));
+        Page<Guestbook> result = guestbookRepository.findAll(builder, pageable);
+        result.stream().forEach(guestbook -> {
+            System.out.println(guestbook);
         });
     }
 }
---
+++ src/test/java/com/example/guestbook/service/GuestbookServiceTest.java
@@ -0,0 +1,37 @@
+package com.example.guestbook.service;
+
+import com.example.guestbook.dto.GuestbookDTO;
+import com.example.guestbook.dto.PageRequestDTO;
+import com.example.guestbook.dto.PageResultDTO;
+import com.example.guestbook.entity.Guestbook;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GuestbookServiceTest {
+    @Autowired
+    private GuestbookService service;
+
+    @Test
+    public void testRegister(){
+        GuestbookDTO guestbookDTO = GuestbookDTO.builder()
+                .title("Sample Title...")
+                .content("Sample Content...")
+                .writer("userSample")
+                .build();
+        System.out.println(service.register(guestbookDTO));
+    }
+
+    @Test
+    public void testList(){
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder()
+                .page(1)
+                .size(10)
+                .build();
+        PageResultDTO<GuestbookDTO, Guestbook> resultDTO = service.getList(pageRequestDTO);
+        for (GuestbookDTO guestbookDTO : resultDTO.getDtoList()){
+            System.out.println(guestbookDTO);
+        }
+    }
+}
