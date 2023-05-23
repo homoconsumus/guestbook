24b2d1fb8751d1d795299c90069e87bcd2a813cf
homoconsumus <rilid@naver.com>
2023-05-23 16:34:01 +0900

    0523

--- src/main/java/com/example/guestbook/controller/GuestBookController.java
+++ src/main/java/com/example/guestbook/controller/GuestBookController.java
@@ -1,5 +1,6 @@
 package com.example.guestbook.controller;
 
+import com.example.guestbook.dto.GuestbookDTO;
 import com.example.guestbook.dto.PageRequestDTO;
 import com.example.guestbook.service.GuestbookService;
 import lombok.RequiredArgsConstructor;
@@ -7,7 +8,9 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/guestbook")
@@ -23,5 +26,18 @@ public class GuestBookController {
     public void list(PageRequestDTO pageRequestDTO, Model model){
         log.info("페이지 요청 정보: " + pageRequestDTO);
         model.addAttribute("result", service.getList(pageRequestDTO));
+    }
+
+    // 등록화면을 보여주는 메소드 Get방식
+    @GetMapping("/register")
+    public void register(){
+        log.info("show register");
+    }
+    // 등록처리 후 목록페이지로 이동 Post방식
+    @PostMapping("/register")
+    public String registerPost(GuestbookDTO dto, RedirectAttributes redirectAttributes){
+        log.info("등록처리 후 목록페이지 리다이렉트");
+        Long gno = service.register(dto);
+        return "redirect:/guestbook/list";
     }
 }
